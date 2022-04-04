b5ff822e1df588c8acb7f2ad463f0e73fcbc03b5
HP <HP@DESKTOP-N09J79Q.mshome.net>
2022-04-04 23:47:48 +0530

    initial commit

---
+++ helloworld/src/test/java/com/urs/sample/helloworld/HelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urs.sample.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
